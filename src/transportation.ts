313109fca8cafa619cb6663930c5a19ee068149e
Nattatorn Yucharoen <nattatorn_yuc@true-e-logistics.com>
2021-03-22 21:08:13 +0700

    linter

--- src/transportation.ts
+++ src/transportation.ts
@@ -1,35 +1,35 @@
-export type LatLng = [number, number]
+export type LatLng = [number, number];
 
 export interface ITransportationStrategy {
-  goto(from: LatLng, to: LatLng): void
+  goto(from: LatLng, to: LatLng): void;
 }
 
 export class Transportation {
   constructor(private strategy: ITransportationStrategy) {}
 
   public setStrategy(strategy: ITransportationStrategy): void {
-    this.strategy = strategy
+    this.strategy = strategy;
   }
 
   public goto(from: LatLng, to: LatLng): void {
-    this.strategy.goto(from, to)
+    this.strategy.goto(from, to);
   }
 }
 
 export class PersonalCar implements ITransportationStrategy {
   public goto(from: LatLng, to: LatLng): void {
-    console.log(`PersonalCar: ${from} ${to}`)
+    console.log(`PersonalCar: ${from} ${to}`);
   }
 }
 
 export class TaxiCab implements ITransportationStrategy {
   public goto(from: LatLng, to: LatLng): void {
-    console.log(`TaxiCab: ${from} ${to}`)
+    console.log(`TaxiCab: ${from} ${to}`);
   }
 }
 
 export class CityBus implements ITransportationStrategy {
   public goto(from: LatLng, to: LatLng): void {
-    console.log(`CityBus: ${from} ${to}`)
+    console.log(`CityBus: ${from} ${to}`);
   }
 }
